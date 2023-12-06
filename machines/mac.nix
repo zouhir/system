d6a06286560fc4c40e3dadfc64015f2042a372d4
Zouhir Chahoud <zouhir@users.noreply.github.com>
2023-12-05 19:37:19 -0800

    partial macos configs

--- machines/mac.nix
+++ machines/mac.nix
@@ -3,40 +3,37 @@
   nix.package = pkgs.nix;
   nix.settings.trusted-users = [ "root" "zouhir" ];
 
-  homebrew = {
-    enable = true;
-    casks = [
-      "google-chrome"
-    ];
-  };
-
   system = {
     defaults = {
       dock = {
         autohide = true;
-      };
-      NSGlobalDomain = {
+        mineffect = "scale"
+        enable-spring-load-actions-on-all-items = true;
+        minimize-to-application = true;
+        show-process-indicators = true;
+        mouse-over-hilite-stack = true;
+        tilesize = 32;
       };
       finder = {
+        DisableAllAnimations = true;
+        NewWindowTarget = "PfDe";
+        NewWindowTargetPath = "file://${HOME}/Desktop/";
+        AppleShowAllExtensions = true;
+        ShowStatusBar = true;
+        ShowPathbar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
       };
-      CustomUserPreferences = {
-        "com.apple.NetworkBrowser" = { "BrowseAllInterfaces" = true; };
-        "com.apple.screensaver" = {
-          "askForPassword" = true;
-          "askForPasswordDelay" = 0;
-        };
-        "com.apple.trackpad" = {  };
-        "com.apple.mouse" = {  };
-        "com.apple.desktopservices" = {  };
-        "com.apple.LaunchServices" = {  };
-        "com.apple.finder" = {
-        };
-        "NSGlobalDomain" = {
-        };
-        "com.apple.ImageCapture" = { "disableHotPlug" = true; };
-      };
+      NSGlobalDomain = {
+        "com.apple.springing.enabled" = true;
+        "com.apple.springing.delay" = 0;
+        
+      }
     };
   };
+
 	environment.shells = with pkgs; [ bashInteractive zsh ];
   users.users.zouhir = {
 		shell = pkgs.bashInteractive;
