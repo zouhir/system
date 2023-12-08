499a0d8e54f0986c1c285c2e48607d849358ff08
Zouhir Chahoud <zouhir@users.noreply.github.com>
2023-12-07 21:38:16 -0800

    bash updates preparing for downstram work

--- modules/bash/default.nix
+++ modules/bash/default.nix
@@ -1,11 +1,30 @@
-{ lib, ... }:
-let
-  bashExtra = [ ./options.bash ./prompt.bash ];
-  bashExtraContents = map builtins.readFile bashExtra;
-in {
+{ lib, ... }: {
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    initExtra = lib.concatStringsSep "\n" bashExtraContents;
+    shellOptions = [
+      # Autocorrect typos in path names when using `cd`
+      "cdspell"
+
+      # For correcting minor spelling errors in directory names
+      "dirspell"
+
+      # Append to history file rather than replacing it.
+      "histappend"
+
+      # check the window size after each command and, if
+      # necessary, update the values of LINES and COLUMNS.
+      "checkwinsize"
+
+      # Case-insensitive globbing (used in pathname expansion)
+      "nocaseglob"
+
+      # Warn if closing shell with running jobs.
+      "checkjobs"
+    ];
+
+    shellAliases = (import ./aliases/common.nix); 
+
+    initExtra = builtins.readFile ./prompt.bash;
   };
 }
